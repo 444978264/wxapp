2ea5e19823f661a00be34be7a05a4a884b1636bc
lxt <972283992@qq.com>
2017-12-23 14:42:07 +0800

    不推送

--- src/pages/area/area.js
+++ src/pages/area/area.js
@@ -9,6 +9,7 @@ extend({
     showCity: false,
     deliveryCity: []
   },
+  idx: '',
   onLoad() {
     this.fetch();
   },
@@ -16,12 +17,57 @@ extend({
     return true;
   },
   showBtn(e) {
-    var idx = e.currentTarget.dataset.idx;
-    var status = this.data.datas[idx].toggle;
-    this.data.datas[idx].toggle = !status;
+    this.idx = e.currentTarget.dataset.idx;
+    var status = this.data.datas[this.idx].toggle;
+    this.data.datas[this.idx].toggle = !status;
     this.setData({
       datas: this.data.datas
     })
+  },
+  selectAll(e) {
+    this.idx = e.currentTarget.dataset.idx;
+    this.data.datas[this.idx].toggle = true;
+    var is_checked = this.data.datas[this.idx].checked;
+    this.data.datas[this.idx].checked = !is_checked;
+    for (var i = 0; i < this.data.datas[this.idx].city.length; i++) {
+      if (!is_checked) {
+        this.data.datas[this.idx].city[i].checked = true;
+      } else {
+        this.data.datas[this.idx].city[i].checked = false;
+      }
+    }
+    this.setData({
+      datas: this.data.datas
+    })
+  },
+  checkCity(e) {
+    var arr = [];
+    var childidx = e.currentTarget.dataset.childidx;
+
+    
+
+    // var status = this.data.datas[this.idx].city[childidx].checked;
+    // this.data.datas[this.idx].city[childidx].checked = !status;
+    // for (var i = 0; i < this.data.datas[this.idx].city.length; i++) {
+    //   arr.push(this.data.datas[this.idx].city[i].checked);
+    // }
+    // if (this.contains(arr, false)) {
+    //   this.data.datas[this.idx].checked = false;
+    // } else {
+    //   this.data.datas[this.idx].checked = true;
+    // }
+    // this.setData({
+    //   datas: this.data.datas
+    // })
+  },
+  contains(arr, val) {  
+    var i = arr.length;  
+    while(i--) {
+      if (arr[i] === val) {
+        return true;
+      }
+    }  
+    return false;  
   },
   checkboxChange(e) {
     this.setData({
@@ -44,17 +90,31 @@ extend({
       var listData = this.data.cityArr;
       for (var i = 0; i < listData.length; i++) {
         listData[i]['toggle'] = false;
+        listData[i]['checked'] = false;
+        for (var k = 0; k < listData[i].city.length; k ++) {
+          listData[i].city[k]['checked'] = false;
+        }
       }
+
       this.setData({
         datas: listData
       })
     });
   },
   delivery() {
+
+    console.log(this.data.deliveryCity, "deliveryCity");
+
+
     if (this.data.deliveryCity.length == 0) {
       this.alert('请选择投放城市','warn');
       return;
     }
+
+    
+
+    return;
+
     this.setItem('deliveryCity', this.data.deliveryCity);
     this.goback();
   }
